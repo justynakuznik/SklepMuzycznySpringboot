781cb095c395d6aee50e6a06ca3296dae59e910c
Justyna Kuźnik <jkuznik@JustynaKs-MacBook-Pro.local>
2022-09-14 20:58:53 +0200

    first commit sprongboot

---
+++ src/main/java/com/sklepmuzyczny/Sklep/SklepApplication.java
@@ -0,0 +1,14 @@
+package com.sklepmuzyczny.Sklep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepApplication.class, args);
+
+	}
+
+}
